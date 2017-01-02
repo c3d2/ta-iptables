5604b50b8385c96c981f4aae9ccd0f7babfac06e
Jörg Thalheim <joerg@higgsboson.tk>
2017-01-02 15:06:57 +0100

    final content

--- network.sh
+++ network.sh
@@ -74,9 +74,11 @@ ip link add br0 type bridge
 ip link set router master br0
 ip link set router up
 ip link set br0 up
-# for debugging
-#ip addr add 172.22.99.250/24 dev br0
-#ip addr add fd43::1/64 dev br0
+ip link set enp0s25 master br0
 
 ip route a fd42::1/64 via fe80::92 dev br0
-ip route add 192.168.42.0/24 via 172.22.99.249 dev br0
+ip addr add fd43::1/64 dev br0
+ip a flush dev enp0s25
+#ip a a 172.22.99.128/24 dev br0
+#ip r a default via 172.22.99.4 dev br0 metric 256
+#ip route add 192.168.42.0/24 via 172.22.99.249 dev br0
